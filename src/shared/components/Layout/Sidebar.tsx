711ed2d1bb42300374140db8f08357aa9d38e390
Jesus Lopez <jjezzlopezz@hotmail.com>
2022-03-09 11:53:35 -0400

    layout base added

--- src/shared/components/Layout/Sidebar.tsx
+++ src/shared/components/Layout/Sidebar.tsx
@@ -7,7 +7,7 @@ import { Box } from '@mui/system';
 import HomeIcon from '@mui/icons-material/Home';
 import { createStyles, makeStyles } from '@mui/styles';
 import { useLocation, useNavigate } from 'react-router-dom';
-import mainLogo from '../../assets/images/mainLogo.png';
+import MainLogo from '../../assets/images/mainLogo.png';
 import { PRIMARY } from '../../css/theme';
 import SettingsIcon from '@mui/icons-material/Settings';
 
@@ -131,7 +131,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ open, drawerWidth }) => {
         <Box display="flex" flexDirection="column" alignItems="center">
           <img
             style={{ width: '70px', margin: '5px', borderRadius: '6px' }}
-            src={mainLogo}
+            src={MainLogo}
             alt="shawcor-logo"
           />
           <Box className={classes.divider} marginTop={2} marginBottom={2} />
