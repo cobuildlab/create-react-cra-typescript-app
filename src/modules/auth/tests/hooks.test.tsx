fb5419bd5e9843917459276462d9aeab97b2c272
Jesus Lopez <jjezzlopezz@hotmail.com>
2022-03-09 12:14:16 -0400

    test changed

--- src/modules/auth/tests/hooks.test.tsx
+++ src/modules/auth/tests/hooks.test.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import TestRenderer, { ReactTestRendererJSON } from 'react-test-renderer';
 import { MockedProvider, MockedResponse } from '@apollo/client/testing';
-import { Sidebar } from '../../../shared/components/Layout/Sidebar';
+import { Topbar } from '../../../shared/components/Layout/Topbar';
 
 const mocks: ReadonlyArray<MockedResponse> = []; // We'll fill this in next
 
 it('renders without error', () => {
   const node = (
     <MockedProvider mocks={mocks} addTypename={false}>
-      <Sidebar open={true} drawerWidth={38} />
+      <Topbar sidebarOpen={true} drawerWidth={88} />
     </MockedProvider>
   );
 
